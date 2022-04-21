c3fd732b7a643d2250ed71d016e54ea84e9996eb
sam <samueldazadaza@gmail.com>
2022-04-20 19:31:08 -0500

    32. ejemplo practico (propiedades de los componentes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import React from "react"
 import Bienvenida from "./components/Bienvenida";
+import Cards from "./components/Cards";
 
 function App() {
   return (
     <div>
       <h1>Propiedades de los componentes</h1>
-      <Bienvenida nombre="Carlos" apellido="Perez"/>
-      <Bienvenida nombre="Santiago" apellido="Sanchez"/>
-      <Bienvenida nombre="Antonio" apellido="Luna"/>
-      <Bienvenida nombre="Maria" apellido="Vargas"/>
+      <hr />
+
+      <div className="row">
+        <div className="col">
+          <Cards
+            imagen="https://picsum.photos/200"
+            titulo= "Titulo card 1"
+            texto= "Texto de la card 1"
+          />
+        </div>
+        <div className="col">
+          <Cards
+            imagen="https://picsum.photos/200"
+            titulo= "Titulo card 2"
+            texto= "Texto de la card 2"
+          />
+        </div>
+        <div className="col">
+          <Cards
+            imagen="https://picsum.photos/200"
+            titulo= "Titulo card 3"
+            texto= "Texto de la card 3"
+          />
+        </div>
+
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Cards = (props) => {
+    //extraemos todos los props con Object Destructuring
+    const {imagen, titulo, texto} = props
+
+    return (
+        <div className="card" style={{width: "10rem", border: "1px solid red"}}>
+            <img src={imagen} className="card-img-top img-thumbnail"  alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{titulo}</h5>
+                <p className="card-text">{texto}</p>
+                <a href="#" className="btn btn-primary">Ver mas</a>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
